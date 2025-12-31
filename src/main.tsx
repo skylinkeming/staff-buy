3870eb31dbcc744e10b8f8d2f2a0178a7e54af7c
stevelin <germinglin@yahoo.com.tw>
2025-12-31 12:24:19 +0800

    設定router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,51 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/staffbuy",
+    element: (
+      <>
+        <h1>員購頁面</h1>
+      </>
+    ),
+  },
+  {
+    path: "/staffbuy/products",
+    element: (
+      <>
+        <h1>員購商品頁面</h1>
+      </>
+    ),
+  },
+  {
+    path: "/groupbuy",
+    element: (
+      <>
+        <h1>團購頁面</h1>
+      </>
+    ),
+  },
+  {
+    path: "/partyup",
+    element: (
+      <>
+        <h1>揪團頁面</h1>
+      </>
+    ),
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
