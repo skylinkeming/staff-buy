9b8b4bbae9140ddbf98aa6c1254f4027be79e16e
stevelin <germinglin@yahoo.com.tw>
2025-12-31 16:44:23 +0800

    cartsummary加上sticky

--- src/components/business/CartSummary.tsx
+++ src/components/business/CartSummary.tsx
@@ -1,7 +1,9 @@
 export default function CartSummary() {
   return (
     <div className="border-[1px] border-[#D9D9D9] p-[30px] w-[260px] rounded-[10px] ">
-      <div className="bg-[#4F48E5] text-[white] rounded-[15px] px-[60px] py-[8px]">購買商品</div>
+      <div className="bg-[#4F48E5] text-[white] rounded-[15px] px-[60px] py-[8px] text-center cursor-pointer">
+        購買商品
+      </div>
     </div>
   );
 }
--- src/pages/staffbuy/StaffProductPage.tsx
+++ src/pages/staffbuy/StaffProductPage.tsx
@@ -1,5 +1,5 @@
 import CartSummary from "../../components/business/CartSummary";
-import Notice from "../../components/business/Notice";
+import Notice from "../../components/common/Notice";
 import ProductListTable from "../../components/business/ProductListTable";
 
 // interface TableRowData {
@@ -12,73 +12,98 @@ import ProductListTable from "../../components/business/ProductListTable";
 
 export default function StaffProductPage() {
   return (
-    <div className="min-h-[100vh] w-[100vw] relative flex gap-[40px]">
-      <div className="w-[740px] inline-block">
+    <div className="min-h-[100%] w-[100%] relative flex flex-col items-center justify-center gap-[40px] bg-[#FBFBFB]">
+      <div>
         <h2>員購商品</h2>
-        <Notice />
-        <ProductListTable
-          data={[
-            {
-              name: "小籠包",
-              price: 50,
-              stock: 100,
-              quantity: 2,
-              subtotal: 100,
-            },
-            {
-              name: "小籠包",
-              price: 50,
-              stock: 100,
-              quantity: 2,
-              subtotal: 100,
-            },
-            {
-              name: "小籠包",
-              price: 50,
-              stock: 100,
-              quantity: 2,
-              subtotal: 100,
-            },
-            {
-              name: "小籠包",
-              price: 50,
-              stock: 100,
-              quantity: 2,
-              subtotal: 100,
-            },
-            {
-              name: "小籠包",
-              price: 50,
-              stock: 100,
-              quantity: 2,
-              subtotal: 100,
-            },
-            {
-              name: "小籠包",
-              price: 50,
-              stock: 100,
-              quantity: 2,
-              subtotal: 100,
-            },
-            {
-              name: "小籠包",
-              price: 50,
-              stock: 100,
-              quantity: 2,
-              subtotal: 100,
-            },
-            {
-              name: "小籠包",
-              price: 50,
-              stock: 100,
-              quantity: 2,
-              subtotal: 100,
-            },
-          ]}
-        />
-      </div>
-      <div className="sticky w-80 inline-block mt-[75px]">
-        <CartSummary />
+        <div className="flex gap-[40px] ">
+          <div className="w-[740px] inline-block">
+            <Notice />
+            <ProductListTable
+              data={[
+                {
+                  name: "小籠包",
+                  price: 50,
+                  stock: 100,
+                  quantity: 2,
+                  subtotal: 100,
+                },
+                {
+                  name: "小籠包",
+                  price: 50,
+                  stock: 100,
+                  quantity: 2,
+                  subtotal: 100,
+                },
+                {
+                  name: "小籠包",
+                  price: 50,
+                  stock: 100,
+                  quantity: 2,
+                  subtotal: 100,
+                },
+                {
+                  name: "小籠包",
+                  price: 50,
+                  stock: 100,
+                  quantity: 2,
+                  subtotal: 100,
+                },
+                {
+                  name: "小籠包",
+                  price: 50,
+                  stock: 100,
+                  quantity: 2,
+                  subtotal: 100,
+                },
+                {
+                  name: "小籠包",
+                  price: 50,
+                  stock: 100,
+                  quantity: 2,
+                  subtotal: 100,
+                },
+                {
+                  name: "小籠包",
+                  price: 50,
+                  stock: 100,
+                  quantity: 2,
+                  subtotal: 100,
+                },
+                {
+                  name: "小籠包",
+                  price: 50,
+                  stock: 100,
+                  quantity: 2,
+                  subtotal: 100,
+                },
+                {
+                  name: "小籠包",
+                  price: 50,
+                  stock: 100,
+                  quantity: 2,
+                  subtotal: 100,
+                },
+                {
+                  name: "小籠包",
+                  price: 50,
+                  stock: 100,
+                  quantity: 2,
+                  subtotal: 100,
+                },
+                {
+                  name: "小籠包",
+                  price: 50,
+                  stock: 100,
+                  quantity: 2,
+                  subtotal: 100,
+                },
+              ]}
+            />
+          </div>
+          <div className="sticky top-[0px] h-[400px] inline-block">
+            <CartSummary />
+          </div>
+        </div>
       </div>
     </div>
   );
