0c1fd4392a3a2f851eb36d6e79677fa6104aec43
stevelin <germinglin@yahoo.com.tw>
2026-01-14 11:16:59 +0800

    調整登入流程 沒登入成功前不要渲染

--- src/components/AuthWrapper.tsx
+++ src/components/AuthWrapper.tsx
@@ -1,26 +1,39 @@
 import { useEffect } from "react";
-import { Outlet, useSearchParams } from "react-router"; // 確保你有安裝 react-router-dom
+import { Outlet, useSearchParams } from "react-router";
 import { useMutation } from "@tanstack/react-query";
 import { staffbuyApi } from "@/api/staffbuyApi";
+import { Spin } from "antd"; // 假設你用 antd 顯示加載中
 
 export default function AuthWrapper() {
   const [searchParams, setSearchParams] = useSearchParams();
-  const { mutate: autoLogin } = useMutation({
+  const qweValue = searchParams.get("qwe");
+
+  const { mutate: autoLogin, isPending } = useMutation({
     mutationFn: (qwe: string) => staffbuyApi.login({ qwe }),
     onSuccess: (res) => {
+      // 儲存 token
       localStorage.setItem("token", res.data);
-      searchParams.delete("qwe");
-      setSearchParams(searchParams, { replace: true });
+      // 清除網址上的參數
+      const newParams = new URLSearchParams(searchParams);
+      newParams.delete("qwe");
+      setSearchParams(newParams, { replace: true });
     },
   });
 
   useEffect(() => {
-    const qweValue = searchParams.get("qwe");
-
     if (qweValue) {
       autoLogin(qweValue);
     }
-  }, [searchParams, autoLogin]);
+  }, []);
+
+
+  if (qweValue && isPending) {
+    return (
+      <div className="flex h-screen w-screen items-center justify-center">
+        <Spin size="large" tip="自動登入中..." />
+      </div>
+    );
+  }
 
   return <Outlet />;
 }
