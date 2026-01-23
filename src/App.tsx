efe3edda8490fe2735a47b2e4310d5323d5b9bf1
stevelin <germinglin@yahoo.com.tw>
2026-01-23 12:12:35 +0800

    新增首頁樣式

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,35 @@
 import { useNavigate } from "react-router";
 import "./App.css";
+import type { ReactNode } from "react";
+import {
+  ShoppingOutlined,
+} from "@ant-design/icons";
+
 
 function App() {
   const navigate = useNavigate();
+
+  const renderMenuItem = (name: string, link: string, icon: ReactNode) => {
+    return (
+      <div className="flex flex-col items-center cursor-pointer"
+        onClick={() => navigate(link)}>
+        <div className="w-35 h-35 bg-blue-50 rounded-2xl flex items-center justify-center mb-6 text-blue-600">
+          {icon}
+        </div>
+        <div
+          className="text-lg cursor-pointer text-blue-400"
+
+        >
+          {name}
+        </div>
+      </div>
+    )
+  }
+
   return (
-    <div className="w-full flex flex-col items-center justify-center gap-5">
-      <div
-        className="text-lg cursor-pointer text-blue-400"
-        onClick={() => navigate("/staffbuy/purchase")}
-      >
-        員購頁面
-      </div>
-      <div
-        className="text-lg cursor-pointer text-blue-400"
-        onClick={() => navigate("/groupbuy/purchase")}
-      >
-        團購頁面
-      </div>
+    <div className="w-full h-screen flex items-center justify-center items-center gap-5 ">
+      {renderMenuItem("員工頁面", "/staffbuy/purchase", <ShoppingOutlined className="text-5xl" />)}
+      {renderMenuItem("團購頁面", "/groupbuy/purchase", <ShoppingOutlined className="text-5xl" />)}
     </div>
   );
 }
